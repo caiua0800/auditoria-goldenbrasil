6052bcc7707b5c7cbbfd435e8c7e3a583104cb0d
Caiuã Mello <105950039+caiua0800@users.noreply.github.com>
2026-03-09 16:46:13 -0300

    owp

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      /\.ccypher\.com\.br$/,   // qualquer subdomínio de ccypher.com.br
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
 
   await app.listen(3001);
